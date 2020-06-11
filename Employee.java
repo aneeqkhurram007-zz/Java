0ebf6f23043962c885376236fd9ee1a3065547e9
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2020-06-11 20:01:36 +0500

    java

---
+++ Employee.java
@@ -0,0 +1,25 @@
+/**
+ * Employee
+ */
+public class Employee extends Person {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private double annualSalary;
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+}
