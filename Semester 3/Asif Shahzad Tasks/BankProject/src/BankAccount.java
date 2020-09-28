d9f5b8ac12e736dccd80823dd47897d88e0bd049
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-09-28 12:19:23 +0500

    BankAccount 2.0

--- Semester 3/Asif Shahzad Tasks/BankProject/src/BankAccount.java
+++ Semester 3/Asif Shahzad Tasks/BankProject/src/BankAccount.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class BankAccount {
     String name;
     double balance;
+    double amount;
     Scanner input = new Scanner(System.in);
+    int countDep = 0, countWith = 0;
 
-    void deposit(double amount) {
+    void deposit() {
         if (balance >= 100000) {
             balance += amount + (0.01 * amount);
         } else {
@@ -13,7 +15,7 @@ public class BankAccount {
         }
     }
 
-    void withdraw(double amount) {
+    void withdraw() {
         if (balance <= 50000) {
             int choice;
             System.out.println("Are you sure you want to withdraw");
@@ -32,10 +34,46 @@ public class BankAccount {
         System.out.println("Prss 2: To Withdraw an amount.");
         System.out.println("Prss 3: To View the current balance.");
         System.out.println("Prss 4: To Close this program.");
+        int choice;
+        // double amount;
+        choice = input.nextInt();
+        while (choice != 0) {
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount you want to deposit in your account > ");
+                    amount = input.nextDouble();
+                    deposit();
+                    countDep++;
+                    break;
+                case 2:
+                    System.out.print("Enter the amount you want to withdraw in your account > ");
+                    amount = input.nextDouble();
+                    withdraw();
+                    countWith++;
+                    break;
+                case 3:
+                    System.out.println("Your account Details: ");
+                    System.out.println("Your Name is : " + name);
+                    System.out.println("Your current balance is : " + balance);
+                    break;
+                case 4:
+                    accountTrans();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("You entered the wrong choice");
+                    break;
+            }
+            mainDisplay();
+            choice = input.nextInt();
+
+        }
 
     }
 
-    void accountTrans(int countDep, int countWith) {
+    void accountTrans() {
         System.out.println("Account Title is : " + name);
         System.out.println("Total deposits : " + countDep);
         System.out.println("Total withdraws : " + countWith);
--- Semester 3/Asif Shahzad Tasks/BankProject/src/BankAccountTest.java
+++ Semester 3/Asif Shahzad Tasks/BankProject/src/BankAccountTest.java
@@ -9,42 +9,42 @@ public class BankAccountTest {
         System.out.println("Enter amount you want to add: ");
         bankAccount.balance = input.nextDouble();
         bankAccount.mainDisplay();
-        int choice, countDep = 0, countWith = 0;
-        double amount;
-        choice = input.nextInt();
-        while (choice != 0) {
+        // int choice, countDep = 0, countWith = 0;
+        // //double amount;
+        // choice = input.nextInt();
+        // while (choice != 0) {
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter the amount you want to deposit in your account > ");
-                    amount = input.nextDouble();
-                    bankAccount.deposit(amount);
-                    countDep++;
-                    break;
-                case 2:
-                    System.out.print("Enter the amount you want to withdraw in your account > ");
-                    amount = input.nextDouble();
-                    bankAccount.withdraw(amount);
-                    countWith++;
-                    break;
-                case 3:
-                    System.out.println("Your account Details: ");
-                    System.out.println("Your Name is : " + bankAccount.name);
-                    System.out.println("Your current balance is : " + bankAccount.balance);
-                    break;
-                case 4:
-                    bankAccount.accountTrans(countDep, countWith);
-                    System.exit(0);
-                    break;
+        // switch (choice) {
+        // case 1:
+        // System.out.print("Enter the amount you want to deposit in your account > ");
+        // amount = input.nextDouble();
+        // bankAccount.deposit();
+        // countDep++;
+        // break;
+        // case 2:
+        // System.out.print("Enter the amount you want to withdraw in your account > ");
+        // amount = input.nextDouble();
+        // bankAccount.withdraw();
+        // countWith++;
+        // break;
+        // case 3:
+        // System.out.println("Your account Details: ");
+        // System.out.println("Your Name is : " + bankAccount.name);
+        // System.out.println("Your current balance is : " + bankAccount.balance);
+        // break;
+        // case 4:
+        // bankAccount.accountTrans(countDep, countWith);
+        // System.exit(0);
+        // break;
 
-                default:
-                    System.out.println("You entered the wrong choice");
-                    break;
-            }
-            bankAccount.mainDisplay();
-            choice = input.nextInt();
+        // default:
+        // System.out.println("You entered the wrong choice");
+        // break;
+        // }
+        // bankAccount.mainDisplay();
+        // choice = input.nextInt();
 
-        }
+        // }
         input.close();
 
     }
