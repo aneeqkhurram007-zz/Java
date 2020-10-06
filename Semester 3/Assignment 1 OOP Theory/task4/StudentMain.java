5eb480591e6106335ce96828164858e2a1490936
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-10-06 09:49:35 +0500

    9.49

--- Semester 3/Assignment 1 OOP Theory/task4/StudentMain.java
+++ Semester 3/Assignment 1 OOP Theory/task4/StudentMain.java
@@ -4,12 +4,12 @@ public class StudentMain {
 
     public static void main(String[] args) {
 
-        Student st1 = new Student("Aneeq", "FA19-BCS-145", "BCS", "CS", 4.0);
-        Student st2 = new Student("Danyal", "FA19-BCS-060", "BCS", "CS", 3.0);
-        Student st3 = new Student("Saleem", "FA19-BCS-109", "BCS", "CS", 3.7);
-        Student st4 = new Student("Saif", "FA19-BCS-112", "BCS", "CS", 3.5);
-        Student st5 = new Student("Affan", "FA19-BCS-165", "BCS", "CS", 3.7);
-        Student st6 = new Student("Wahab", "FA19-BCS-003", "BCS", "CS", 2.7);
+        Student st1 = new Student("A", "FA19-BCS-001", "BCS", "CS", 4.0);
+        Student st2 = new Student("B", "FA19-BCS-002", "BCS", "CS", 3.0);
+        Student st3 = new Student("C", "FA19-BCS-003", "BCS", "CS", 3.7);
+        Student st4 = new Student("D", "FA19-BCS-004", "BCS", "CS", 3.5);
+        Student st5 = new Student("E", "FA19-BCS-005", "BCS", "CS", 3.7);
+        Student st6 = new Student("F", "FA19-BCS-006", "BCS", "CS", 2.7);
 
         switch (Student.compare(st1.cgpa, st2.cgpa, st3.cgpa, st4.cgpa, st5.cgpa, st6.cgpa)) {
             case 0:
