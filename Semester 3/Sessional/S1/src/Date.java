5b41ea84be368f271991b7d18e878a13a2813af2
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-10-18 13:54:45 +0500

    1.54

--- Semester 3/Sessional/S1/src/Date.java
+++ Semester 3/Sessional/S1/src/Date.java
@@ -8,7 +8,8 @@ public class Date {
     }
 
     public void setDay(int day) {
-        this.day = day;
+        this.day = (day >= 1 && day <= 31) ? (this.day = day) : (this.day = 1);
+
     }
 
     public int getMonth() {
@@ -16,7 +17,7 @@ public class Date {
     }
 
     public void setMonth(int month) {
-        this.month = month;
+        this.month = (month >= 1 && day <= 12) ? (this.month = month) : (this.month = 1);
     }
 
     public int getYear() {
@@ -24,13 +25,13 @@ public class Date {
     }
 
     public void setYear(int year) {
-        this.year = year;
+        this.year = (year >= 1) ? (this.year = year) : (this.year = 1);
     }
 
     public Date(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+        setDay(day);
+        setMonth(month);
+        setYear(year);
     }
 
     public Date() {
