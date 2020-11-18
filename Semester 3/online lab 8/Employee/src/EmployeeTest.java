484046e5f4b4541b8ddfded9053a2b7919e3c3c5
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-11-18 21:39:20 +0500

    9.39

--- Semester 3/online lab 8/Employee/src/EmployeeTest.java
+++ Semester 3/online lab 8/Employee/src/EmployeeTest.java
@@ -1,7 +1,7 @@
 public class EmployeeTest {
     public static void main(String[] args) {
-        HourlyEmployee e1 = new HourlyEmployee("Aneeq", 1, 100);
-        PermanentEmployee e2 = new PermanentEmployee("Murshad", 2, 100);
+        Employee e1 = new HourlyEmployee("Aneeq", 1, 100);
+        Employee e2 = new PermanentEmployee("Murshad", 2, 100);
 
         System.out.println(e1);
         System.out.println(e2);
