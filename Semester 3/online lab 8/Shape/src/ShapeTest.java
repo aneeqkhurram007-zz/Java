bb7e47477bec714265769d448b9e7a0a99bf95d0
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-11-19 16:47:01 +0500

    4.46

--- Semester 3/online lab 8/Shape/src/ShapeTest.java
+++ Semester 3/online lab 8/Shape/src/ShapeTest.java
@@ -3,28 +3,21 @@ import java.util.ArrayList;
 public class ShapeTest {
     public static void main(String[] args) {
 
-        ArrayList<Rectangle> r = new ArrayList<>();
-        ArrayList<Square> s = new ArrayList<>();
+        ArrayList<Shape> r = new ArrayList<>();
 
-        r.add(0, new Rectangle());
-        r.add(1, new Rectangle(2.0, 3.0));
-        r.add(2, new Rectangle("Red", false, 5.0, 3.0));
+        r.add(new Rectangle());
+        r.add(new Rectangle(2.0, 3.0));
+        r.add(new Rectangle("Red", false, 5.0, 3.0));
+        r.add(new Square());
+        r.add(new Square(3.0));
+        r.add(new Square("Green", false, 4.0));
 
-        s.add(0, new Square());
-        s.add(1, new Square(3.0));
-        s.add(2, new Square("Green", true, 4.0));
+        for (Shape shape : r) {
 
-        System.out.println("\tRectangle");
-        for (Rectangle rectangle : r) {
-            rectangle.getArea();
-            rectangle.getPerimeter();
-            System.out.println(rectangle);
-        }
-        System.out.println("\tSquare");
-        for (Square rectangle : s) {
-            rectangle.getArea();
-            rectangle.getPerimeter();
-            System.out.println(rectangle);
+            System.out.println("\nShape # " + (r.indexOf(shape) + 1));
+            System.out.println(" Area is = " + shape.getArea());
+            System.out.println(" Perimeter is = " + shape.getPerimeter());
+            System.out.println(shape);
         }
 
     }
