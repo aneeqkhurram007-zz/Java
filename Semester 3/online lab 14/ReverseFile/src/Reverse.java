7fcd676aa02944b5aa7fa036018645883e283b8e
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-12-27 16:18:40 +0500

    4.18

--- Semester 3/online lab 14/ReverseFile/src/Reverse.java
+++ Semester 3/online lab 14/ReverseFile/src/Reverse.java
@@ -1,28 +1,45 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class Reverse {
 
     public static void main(String[] args) throws IOException {
-        FileWriter fileWriter = new FileWriter("data.txt");
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write("1 2 3 4 5 6");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data.txt"));
+
+        bufferedWriter.write("6 5 4 3 2 1");
         bufferedWriter.close();
-        FileReader fileReader = new FileReader("data.txt");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        ArrayList<Integer> integers = new ArrayList<>();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("data.txt"));
+
+        ArrayList<Character> characters = new ArrayList<>();
         int i, count = 0;
+
         do {
             i = bufferedReader.read();
             if (i != -1) {
                 char c = (char) i;
-                // integers.add(Character.getNumericValue(c));
-                integers.add(i);
-                System.out.println(integers.get(count));
+                characters.add(c);
+                System.out.println(characters.get(count));
                 count++;
             }
         } while (i != -1);
+
         bufferedReader.close();
+
+        Collections.reverse(characters);
+        Iterator<Character> it = characters.iterator();
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt"));
+
+        while (it.hasNext()) {
+            Character ch = it.next();
+            printWriter.write(String.valueOf(ch));
+            System.out.println(ch);
+        }
+        printWriter.flush();
+        printWriter.close();
     }
 
 }
