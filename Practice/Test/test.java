2bb35f261ae927bcb45f8d79178a1bea690dbfec
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-21 12:22:05 +0500

    12.22

---
+++ Practice/Test/test.java
@@ -0,0 +1,13 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 148; i += 3) {
+            sum += i;
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
