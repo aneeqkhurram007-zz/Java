487e0dfd0e963a8925bb8a77c0dbdfecde724c04
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-27 12:37:54 +0500

    12.37

--- Practice/Test/Nested Loops/star3.java
+++ Practice/Test/Nested Loops/star3.java
@@ -1,22 +1,32 @@
 public class star3 {
     public static void main(String[] args) {
-        int cx = 0;
-        int x = 1;
-        while (cx < 5) {
-            int space = 4 - cx;
-            while (space >= 0) {
+        // int cx = 0;
+        // int x = 1;
+        // while (cx < 5) {
+        // int space = 4 - cx;
+        // while (space >= 0) {
+        // System.out.print(' ');
+        // space--;
+        // }
+        // int total = cx + x;
+        // int bx = 0;
+        // while (bx < total) {
+        // System.out.print('*');
+        // bx++;
+        // }
+        // System.out.println();
+        // cx++;
+        // x++;
+        // }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j >= i; j--) {
                 System.out.print(' ');
-                space--;
             }
-            int dl = cx + x;
-            int b = 0;
-            while (b < dl) {
-                System.out.print('*');
-                b++;
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
-            cx++;
-            x++;
         }
     }
 }
